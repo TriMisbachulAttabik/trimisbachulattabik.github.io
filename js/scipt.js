be007ef18e177449062e8a5e180bcccba44c3f9c
TriMisbachulAttabik <attabik.1402@gmail.com>
2022-06-29 03:33:08 +0700

    commit2

--- js/scipt.js
+++ js/scipt.js
@@ -1,28 +1,36 @@
-var nav_sticky = document.querySelector('nav');
+
+
+
+var nav_items = document.querySelector('.nav_items');
+var hamburger_menu = document.querySelector('.hamburger_menu');
+var items = document.querySelectorAll('.nav_items a');
+
+
+hamburger_menu.addEventListener('click', function(){
+    hamburger_menu.classList.toggle('hamburger_menu_action');
+    nav_items.classList.toggle('nav_items_slide');
+});
+
+items.forEach((item) => {
+	item.addEventListener('click', () => {
+        hamburger_menu.classList.toggle('hamburger_menu_action');
+        nav_items.classList.toggle('nav_items_slide');
+	});
+});
+
+// Navbar Sticky
+
+var nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function(){
     var scroll_position = this.window.scrollY;
-    if (scroll_position > 0) {
-        nav_sticky.classList.add('nav-sticky');       
+    if (scroll_position > 4) {
+        nav.classList.add('nav_sticky');
+        
     } else {
-        nav_sticky.classList.remove('nav-sticky');         
-    }
-})
-
-var nav_item = document.querySelector('.nav-item');
-var hamburger_menu = document.querySelector('.hamburger-menu');
-var menu_item = document.querySelectorAll('nav ul li a');
-var brand_invert = document.querySelectorAll('.brand');
-
-hamburger_menu.addEventListener('click', function(){
-    hamburger_menu.classList.toggle('hm-active');
-    nav_item.classList.toggle('nav-slide');
-})
-
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-        nav_item.classList.toggle('nav-slide');
-        hamburger_menu.classList.toggle('hm-active');
-	});
+        nav.classList.remove('nav_sticky');   
+    } 
 });
+
+
 
