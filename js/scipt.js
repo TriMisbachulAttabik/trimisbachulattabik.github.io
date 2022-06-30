9dad490e65c065e3f228ef1796447c5335789ae2
TriMisbachulAttabik <attabik.1402@gmail.com>
2022-07-01 00:56:58 +0700

    commit2

--- js/scipt.js
+++ js/scipt.js
@@ -1,36 +1,40 @@
 
 
 
-var nav_items = document.querySelector('.nav_items');
-var hamburger_menu = document.querySelector('.hamburger_menu');
-var items = document.querySelectorAll('.nav_items a');
-
+var hamburger_menu = document.querySelector('.hamburger-menu');
+var nav_items = document.querySelector('.nav-items');
+var items = document.querySelectorAll('.nav-items a');
 
 hamburger_menu.addEventListener('click', function(){
-    hamburger_menu.classList.toggle('hamburger_menu_action');
-    nav_items.classList.toggle('nav_items_slide');
-});
+    hamburger_menu.classList.toggle('hamburger-menu-action');
+    nav_items.classList.toggle('nav-items-slide');
+})
+
+
 
 items.forEach((item) => {
 	item.addEventListener('click', () => {
-        hamburger_menu.classList.toggle('hamburger_menu_action');
-        nav_items.classList.toggle('nav_items_slide');
+        hamburger_menu.classList.toggle('hamburger-menu-action');
+        nav_items.classList.toggle('nav-items-slide');
 	});
 });
+
+
+
 
 // Navbar Sticky
 
 var nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function(){
-    var scroll_position = this.window.scrollY;
-    if (scroll_position > 4) {
-        nav.classList.add('nav_sticky');
+    var scroll_position = window.scrollY;
+    if (scroll_position > 20 ) {
+        nav.classList.add('nav-sticky');
         
     } else {
-        nav.classList.remove('nav_sticky');   
-    } 
-});
-
+        nav.classList.remove('nav-sticky');
+        
+    }
+})
 
 
